1fc9e659774344c8932b5723a82b8ccb9ac2e370
Karoline <karolgribeiro@gmail.com>
2022-05-15 20:57:22 -0300

    save score, use navigate

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -1,8 +1,9 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Movie } from "../../types/movie";
 import { BASE_URL } from "../../utils/requests";
+import { validateEmail } from "../../utils/validateEmail";
 import "./styles.css";
 
 type Props = {
@@ -10,12 +11,41 @@ type Props = {
 };
 
 export const FormCard = ({ movieId }: Props) => {
+  const navigate = useNavigate();
+
   const [movie, setMovie] = useState<Movie>();
+
   useEffect(() => {
     axios.get(`${BASE_URL}/movies/${movieId}`).then((response) => {
       setMovie(response.data);
     });
   }, [movieId]);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const email = (event.target as any).email.value;
+    const score = (event.target as any).score.value;
+    
+    if (!validateEmail(email)){
+      return;
+    }
+    
+    const config: AxiosRequestConfig = {
+      baseURL: BASE_URL,
+      method: 'PUT',
+      url: '/scores',
+      data: {
+        email: email,
+        movieId: movieId,
+        score: score
+      }
+    }
+
+    axios(config).then(response => {
+      navigate("/")
+    })
+  };
 
   return (
     <div className="dsmovie-form-container">
@@ -26,7 +56,7 @@ export const FormCard = ({ movieId }: Props) => {
       />
       <div className="dsmovie-card-bottom-container">
         <h3>{movie?.title}</h3>
-        <form className="dsmovie-form">
+        <form className="dsmovie-form" onSubmit={handleSubmit}>
           <div className="form-group dsmovie-form-group">
             <label htmlFor="email">Informe seu email</label>
             <input type="email" className="form-control" id="email" />
@@ -42,11 +72,9 @@ export const FormCard = ({ movieId }: Props) => {
             </select>
           </div>
           <div className="dsmovie-form-btn-container">
-            <Link to="/">
-              <button type="submit" className="btn btn-primary dsmovie-btn">
-                Salvar
-              </button>
-            </Link>
+            <button type="submit" className="btn btn-primary dsmovie-btn">
+              Salvar
+            </button>
           </div>
         </form>
         <Link to="/">
@@ -56,3 +84,7 @@ export const FormCard = ({ movieId }: Props) => {
     </div>
   );
 };
+function validadeEmail(email: any) {
+  throw new Error("Function not implemented.");
+}
+
