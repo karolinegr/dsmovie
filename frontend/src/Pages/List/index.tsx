c5122053878b9339d2e66c392b8531bad35e6d44
Karoline <karolgribeiro@gmail.com>
2022-05-15 19:39:06 -0300

    useState, useEffect

--- frontend/src/Pages/List/index.tsx
+++ frontend/src/Pages/List/index.tsx
@@ -1,17 +1,26 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
+import { BASE_URL } from "../../utils/requests";
 import { Pagination } from "../../components/Pagination";
 import { Moviecard } from "../../components/Moviecard";
 import { Container, Row, Col } from "react-bootstrap";
-import { BASE_URL } from "../../utils/requests";
+import { MoviePage } from "../../types/movie";
+
 
 export const List = () => {
 
-  //TESTE
-  axios.get(`${BASE_URL}/movies?size=12&page=0`).then(
-    response => {
-      console.log(response.data)
-    }
-  )
+  const [pageNumber, setPageNumber] = useState(0);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=1`)
+      .then(
+        response => {
+          const data= response.data as MoviePage;
+          console.log(data)
+          setPageNumber(data.number)
+        }
+      )
+  }, [])
 
   return (
     <>
